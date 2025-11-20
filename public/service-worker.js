79adc90a8888efb7a88a1f4190edc37b1d954c39
matanlord <121100514+matanlord@users.noreply.github.com>
2025-11-20 16:21:14 +0200

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,7 @@
-// service worker
+// Simple placeholder service worker (not required for basic live app)
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
